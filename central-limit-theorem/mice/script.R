395f9a37f110b0ee064a86a40e919fd42847177b
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2018-02-09 19:42:40 -0200

    Find the distribution of averages for different sample sizes
    First we take 5 samples and record the mean 1000 times,
    then we repeat the experiment with 50 samples.  We then look
    at the different distributions using histograms.

--- central-limit-theorem/mice/script.R
+++ central-limit-theorem/mice/script.R
@@ -3,4 +3,23 @@ url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extd
 filename <- basename(url)
 download(url, destfile=filename)
 x <- unlist( read.csv(filename) )
-hist(x)
+
+set.seed(1)
+
+sample_size <- 5
+n <- 1000
+avgs1 <- vector('numeric', n)
+for(i in 1:n) {
+    avgs1[i] <- mean(sample(x, sample_size))
+}
+
+sample_size <- 50
+n <- 1000
+avgs2 <- vector('numeric', n)
+for(i in 1:n) {
+    avgs2[i] <- mean(sample(x, sample_size))
+}
+
+cdf1 <- ecdf(avgs1)
+cdf2 <- ecdf(avgs2)
+
