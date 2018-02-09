cac60d27667f9585475b707720fc9623d643d876
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2018-02-09 20:19:02 -0200

    Find the average bodyweight of control males

--- central-limit-theorem/mice-pheno/script.R
+++ central-limit-theorem/mice-pheno/script.R
@@ -5,3 +5,7 @@ download(url, destfile=filename)
 dat <- read.csv(filename)
 
 dat <- na.omit(dat)
+
+library(dplyr)
+x <- dat %>% filter(Sex=='M') %>% filter(Diet=='chow') %>% select(Bodyweight) %>% unlist
+print(mean(x))
