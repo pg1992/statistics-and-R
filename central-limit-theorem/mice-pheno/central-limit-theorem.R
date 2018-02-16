7835cf46fc23f69e9d59f5f4cb9a43071b7208c1
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2018-02-16 09:47:08 -0200

    Add script to download the necessary data for the Central Limit Theorem exercise

---
+++ central-limit-theorem/mice-pheno/central-limit-theorem.R
@@ -0,0 +1,5 @@
+library(downloader) 
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/mice_pheno.csv"
+filename <- basename(url)
+download(url, destfile=filename)
+dat <- na.omit( read.csv(filename) )
