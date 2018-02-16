c17d13c354b1a439dfbab39006d971572429614a
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2018-02-16 10:08:38 -0200

    Solve the fourth exercise

--- central-limit-theorem/mice-pheno/central-limit-theorem.R
+++ central-limit-theorem/mice-pheno/central-limit-theorem.R
@@ -1,9 +1,11 @@
-library(downloader) 
+library(rafalib)
+library(dplyr)
+library(downloader)
+
 url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/mice_pheno.csv"
 filename <- basename(url)
 download(url, destfile=filename)
 dat <- na.omit( read.csv(filename) )
-
 
 # If a list of numbers has a distribution that is well approximated by the
 # normal distribution, what proportion of these numbers are within one standard
@@ -17,3 +19,9 @@ prop_2sd <- 2*pnorm(2) - 1
 # What proportion of these numbers are within three standard deviations away
 # from the list's average?
 prop_3sd <- 2*pnorm(3) - 1
+
+# Define y to be the weights of males on the control diet. What proportion of
+# the mice are within one standard deviation away from the average weight
+# (remember to use popsd for the population sd)?
+y <- dat %>% filter(Sex=='M') %>% filter(Diet=='chow') %>% select(Bodyweight) %>% unlist
+prop_y_1sd <- mean(abs(y - mean(y)) <= popsd(y))
