eef9618ae546920a99b679396a81b7cade0d089e
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2018-02-16 10:11:25 -0200

    Solve the fifth problem of the CLT exercises

--- central-limit-theorem/mice-pheno/central-limit-theorem.R
+++ central-limit-theorem/mice-pheno/central-limit-theorem.R
@@ -26,8 +26,13 @@ prop_3sd <- 2*pnorm(3) - 1
 y <- dat %>% filter(Sex=='M') %>% filter(Diet=='chow') %>% select(Bodyweight) %>% unlist
 prop_y_1sd <- mean(abs(y - mean(y)) <= popsd(y))
 
+# What proportion of these numbers are within two standard deviations away from
+# the list's average?
+prop_y_2sd <- mean(abs(y - mean(y)) <= 2*popsd(y))
+
 # Answers per se
 print(paste('A  1 - Proportion inside 1 sigma:', prop_1sd))
 print(paste('A  2 - Proportion inside 2 sigma:', prop_2sd))
 print(paste('A  3 - Proportion inside 3 sigma:', prop_3sd))
 print(paste('A  4 - Proportion inside 1 sigma of y:', prop_y_1sd))
+print(paste('A  5 - Proportion inside 2 sigma of y:', prop_y_2sd))
