49bedbd6f19fad418c56207f441b7ad9a378eaf4
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2018-02-16 09:50:49 -0200

    Solve the first exercise from the Central Limit Theorem exercise

--- central-limit-theorem/mice-pheno/central-limit-theorem.R
+++ central-limit-theorem/mice-pheno/central-limit-theorem.R
@@ -3,3 +3,9 @@ url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extd
 filename <- basename(url)
 download(url, destfile=filename)
 dat <- na.omit( read.csv(filename) )
+
+
+# If a list of numbers has a distribution that is well approximated by the
+# normal distribution, what proportion of these numbers are within one standard
+# deviation away from the list's average?
+prop_1sd <- 2*pnorm(1) - 1
